6f53f01794995d151e47b6c543c856d6052e0492
Roman Lemus <roman09071999@gmail.com>
2023-01-26 12:35:56 -0600

    Added loader if the user is logged in to the protected routes

--- src/components/ProtectedRoutes.tsx
+++ src/components/ProtectedRoutes.tsx
@@ -1,11 +1,11 @@
-import { Navigate, Outlet } from "react-router-dom";
-
+import { Outlet } from "react-router-dom";
+import Loader from "./Loader";
 interface ProtectedRoutesProps {
   isAuth: boolean;
 }
 
 const ProtectedRoutes = ({ isAuth }: ProtectedRoutesProps) => {
-  return isAuth ? <Outlet /> : <Navigate to="/login" />;
+  return <>{isAuth ? <Outlet /> : <Loader />}</>;
 };
 
 export default ProtectedRoutes;
