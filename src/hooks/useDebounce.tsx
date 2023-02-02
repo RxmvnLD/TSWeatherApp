05c3ef4e3ce526f871d1cac0f04d62ed62acfe24
Roman Lemus <roman09071999@gmail.com>
2023-02-01 18:37:20 -0600

    TS Types from previous commit updated

--- src/hooks/useDebounce.tsx
+++ src/hooks/useDebounce.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useMemo } from "react";
 import debounce from "../helpers/debounce";
 
-const useDebounce = <A = unknown, R = void>(
-  fn: (args: A) => R,
+const useDebounce = <A extends any[], R = void>(
+  fn: (...args: A) => R,
   delay: number
-): ((args: A) => Promise<R>) => {
+): ((...args: A) => Promise<R>) => {
   const [debouncedFn, teardown] = useMemo(
     () => debounce(fn, delay),
     [fn, delay]
